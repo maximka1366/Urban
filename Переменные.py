e5542d9c94a7b3ebd4644a19aa3afa8f3add975f
maximka1366 <maximka66@mail.ru>
2024-07-02 09:59:44 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,6 @@
+kol_dz =12
+kol_hour = 1.5
+name_kurs = 'Python'
+hour_one_dz = kol_hour / kol_dz
+print('Курс:', name_kurs, ', всего задач:', kol_dz ,', затрачено часов:', kol_hour, ', среднее время выполнения:', hour_one_dz)
+
