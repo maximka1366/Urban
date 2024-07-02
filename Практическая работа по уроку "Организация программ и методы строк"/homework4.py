e9f35d88ad29bde213c75b0e42c650c515459279
maximka1366 <maximka66@mail.ru>
2024-07-02 22:40:44 +0300

    Add files via upload

---
+++ Практическая работа по уроку "Организация программ и методы строк"/homework4.py
@@ -0,0 +1,11 @@
+my_string = input('Введите строку:')
+print('Строка в верхнем регистре:', my_string.upper())
+print('Строка в нижнем регистре:', my_string.lower())
+print('Строка без пробелов:', my_string.replace(' ',''))
+print('Первый символ строки:', my_string[0])
+print('Последний символ строки:', my_string[-1])
+
+
+
+
+
