ed0c7c5333b0eb80812b0f127d909c96ddcb03da
maximka1366 <maximka66@mail.ru>
2024-07-05 17:34:09 +0300

    Add files via upload

---
+++ Практическое задание по теме: "Неизменяемые и изменяемые объекты. Кортежи и списки"/homework5.py
@@ -0,0 +1,5 @@
+immutable_var =  (1, 2, 3, 'Hello', True, 'World', False)
+print('immutable_var' , type(immutable_var ), ':', immutable_var)
+# immutable_var[2] = 5 Программа выдает ошибку так как Коттедж ботносится к низменяемуму типу лаееыъ
+immutable_list = [1, 2, 3, 'Hello', True, 'World', False]
+print('immutable_list' , type(immutable_list), ':', immutable_list)
